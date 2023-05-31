d3665104f9f7a1d0f9b61c0144fab20d162817e1
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-05-31 11:23:48 +0530

    added backend url

--- src/utils/backendURL.js
+++ src/utils/backendURL.js
@@ -1,3 +1,4 @@
-const BACKEND_URL = "http://localhost:5000/api";
+// const BACKEND_URL = "http://localhost:5000/api";
+const BACKEND_URL = "https://file-uploader-server-tau.vercel.app";
 
 export default BACKEND_URL;
