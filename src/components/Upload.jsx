7468c85626fb33e737e5162c8b807c129e60b8e4
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-05-31 10:50:40 +0530

    added client files for file uploader

---
+++ src/components/Upload.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import BACKEND_URL from "../utils/backendURL";
+const Upload = () => {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => [setFile(e.target.files[0])];
+
+  const submitFile = async (e) => {
+    e.preventDefault();
+    if (!file) return;
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await axios.post(`${BACKEND_URL}/uploadFile`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log(res);
+  };
+  return (
+    <div>
+      <form onSubmit={submitFile}>
+        <label htmlFor="">Choose a file</label>
+        <input type="file" onChange={handleFileChange} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Upload;
---
+++ src/utils/backendURL.js
@@ -0,0 +1,3 @@
+const BACKEND_URL = "http://localhost:5000/api";
+
+export default BACKEND_URL;
