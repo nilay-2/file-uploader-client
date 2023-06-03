3028671b0f67e9146e44bbcea6a685b8c489cc80
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-06-03 23:21:27 +0530

    added backend url

--- src/utils/backendURL.js
+++ src/utils/backendURL.js
@@ -1,4 +1,5 @@
-const BACKEND_URL = "http://localhost:5000/api";
-// const BACKEND_URL = "https://file-uploader-server-mern.vercel.app/api";
+// const BACKEND_URL = "http://localhost:5000/api";
+const BACKEND_URL =
+  "https://file-uploader-server-production.up.railway.app/api";
 
 export default BACKEND_URL;
